0e9677a553c60788a05e82ff0af38d73df9f1377
Avinash372 <avinash.sahu3123@gmail.com>
2022-05-01 17:37:59 +0530

    project is completed

--- js/script.js
+++ js/script.js
@@ -16,16 +16,30 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-// starting condition
-score0El.textContent = Number(0);
-score1El.textContent = Number(0);
-diceEl.classList.add("hidden");
+// function to initialize game
 
-const score = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let score, currentScore, activePlayer, playing;
 
+function init() {
+  score = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  // starting condition
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+}
+init();
 // switch player
 
 const switchPlayer = () => {
@@ -92,3 +106,7 @@ btnHold.addEventListener("click", () => {
     }
   }
 });
+
+// reset functionality
+
+btnNew.addEventListener("click", init);
